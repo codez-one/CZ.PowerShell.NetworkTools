d838a408cbea1a4fe806d00decd31a8688b5bd9b
Paul Jeschke <Paul-Jeschke@outlook.com>
2021-05-08 19:38:52 +0200

    remove token from help uri
    just always push your credentials to know something is wrong 😁

--- src/Test-ProxyConfiguration.ps1
+++ src/Test-ProxyConfiguration.ps1
@@ -1,6 +1,6 @@
 Set-StrictMode -Version Latest;
 function Test-ProxyConfiguration {
-    [CmdletBinding(HelpURI="https://raw.githubusercontent.com/codez-one/CZ.PowerShell.NetworkTools/main/docs/Commands/Test-ProxyConfiguration.md?token=ACWEZ3JHCNHLJL4YVKADZEDAS3EVM")]
+    [CmdletBinding(HelpURI="https://raw.githubusercontent.com/codez-one/CZ.PowerShell.NetworkTools/main/docs/Commands/Test-ProxyConfiguration.md")]
     param (
         # The target URL that you are trying to test.
         [Parameter(
